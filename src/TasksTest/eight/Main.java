533c3acf507d9c329fb350978baeafa8d78c7dcf
jasen <jasenkoff.mack@yandex.ru>
2026-08-26 20:54:26 +0300

    homework

--- src/TasksTest/eight/Main.java
+++ src/TasksTest/eight/Main.java
@@ -1,5 +1,6 @@
 package TasksTest.eight;
 
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -11,7 +12,7 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
         int number = random.nextInt(20) + 1;
-        for (int i = 0; i > -1; i++) {
+        while (true) {
             int userNumber = scanner.nextInt();
             if (number == userNumber) {
                 break;
@@ -25,5 +26,40 @@ public class Main {
             }
         }
         System.out.println("Поздраваляю вы угадали мое число: " + number);
+
+        // Даны две строки. Нужно проверить, являются ли они анаграммами (состоят из одних и тех же букв в любом порядке).
+        String s1 = "CAT";
+        String s2 = "tac";
+        String str1 = s1.replaceAll("\\s+", "").toLowerCase();
+        String str2 = s2.replaceAll("\\s+", "").toLowerCase();
+
+        if (str1.length() != str2.length()) {
+            System.out.println("not anagramme");
+        } else {
+            char[] arr1 = str1.toCharArray();
+            char[] arr2 = str2.toCharArray();
+            Arrays.sort(arr1);
+            Arrays.sort(arr2);
+            boolean result = Arrays.equals(arr1, arr2);
+            if (result) {
+                System.out.println("anagramme");
+            } else {
+                System.out.println("not anagramme");
+            }
+        }
+
+        //Даны два отсортированных по возрастанию массива целых чисел. Нужно объединить их в один отсортированный массив.
+        int[] arr1 = {1, 4, 7, 9, 10, 11, 17};
+        int[] arr2 = {1, 4, 8, 10, 34, 71};
+        int[] arrResult = new int[arr1.length + arr2.length];
+        for (int i = 0; i < arr1.length; i++) {
+            arrResult[i] = arr1[i];
+        }
+        for (int i = arr1.length; i < arrResult.length; i++) {
+            arrResult[i] = arr2[i - arr1.length];
+        }
+        Arrays.sort(arrResult);
+        System.out.println(Arrays.toString(arrResult));
     }
 }
+
---
+++ src/TasksTest/nine/Main.java
@@ -0,0 +1,38 @@
+package TasksTest.nine;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Введите сторку со разными скобочками: ");
+        Scanner scanner = new Scanner(System.in);
+        String string = scanner.nextLine();
+        if (isBalanced(string)) {
+            System.out.println("СБАЛАНСИРОВАНА");
+        } else {
+            System.out.println("НЕ СБАЛАНСИРОВАНА");
+        }
+
+    }
+
+    public static boolean isBalanced(String str) {
+        ArrayDeque<Character> deque = new ArrayDeque<>();
+
+        for (Character c : str.toCharArray()) {
+            if (c == '(' || c == '[' || c == '{') {
+                deque.push(c);
+            }
+            if (c == ')' || c == ']' || c == '}') {
+                if (deque.isEmpty()) {
+                    return false;
+                }
+                Character s = deque.pop();
+                if (s == '(' && c != ')') return false;
+                if (s == '[' && c != ']') return false;
+                if (s == '{' && c != '}') return false;
+            }
+        }
+        return deque.isEmpty();
+    }
+}
