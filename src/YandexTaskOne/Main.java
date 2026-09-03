e417cefe6b12b8d56078d49d8770d86ab614927e
jasen <jasenkoff.mack@yandex.ru>
2026-09-03 09:28:35 +0300

    homework

--- src/YandexTaskOne/Main.java
+++ src/YandexTaskOne/Main.java
@@ -1,34 +1,58 @@
 package YandexTaskOne;
 
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] first = br.readLine().split(" ");
-        int n = Integer.parseInt(first[0]);
-        int t = Integer.parseInt(first[1]);
-        int[] rowCnt = new int[n];
-        int[] colCnt = new int[n];
-        int diag1 = 0;
-        int diag2 = 0;
-        String[] nums = br.readLine().split(" ");
-        for (int round = 1; round <= t; round++) {
-            int a = Integer.parseInt(nums[round - 1]);
-            int row = (a - 1) / n;
-            int col = (a - 1) % n;
-            rowCnt[row]++;
-            colCnt[col]++;
-            if (row == col) diag1++;
-            if (row + col == n - 1) diag2++;
-            if (rowCnt[row] == n || colCnt[col] == n || diag1 == n || diag2 == n) {
-                System.out.println(round);
-                return;
+    static long[][] prize;
+    static int n;
+    static int totalPlayers;
+
+    static long[] solve(int l, int r, int h) {
+        if (h == 0) {
+            return new long[]{0};
+        }
+        int mid = (l + r) / 2;
+        long[] left = solve(l, mid, h - 1);
+        long[] right = solve(mid, r, h - 1);
+        int half = 1 << (h - 1);
+        long[] result = new long[1 << h];
+        long maxRightLoss = Long.MIN_VALUE;
+        for (int j = 0; j < half; j++) {
+            long val = right[j] + prize[mid + j][h - 1];
+            if (val > maxRightLoss) maxRightLoss = val;
+        }
+        for (int i = 0; i < half; i++) {
+            result[i] = left[i] + maxRightLoss;
+        }
+        long maxLeftLoss = Long.MIN_VALUE;
+        for (int i = 0; i < half; i++) {
+            long val = left[i] + prize[l + i][h - 1];
+            if (val > maxLeftLoss) maxLeftLoss = val;
+        }
+        for (int j = 0; j < half; j++) {
+            result[half + j] = right[j] + maxLeftLoss;
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        totalPlayers = 1 << n;
+        prize = new long[totalPlayers][n + 1];
+        for (int i = 0; i < totalPlayers; i++) {
+            for (int j = 1; j <= n; j++) {
+                prize[i][j] = sc.nextLong();
             }
         }
-        System.out.println(-1);
+        long[] bestWithoutFinal = solve(0, totalPlayers, n);
+        long answer = Long.MIN_VALUE;
+        for (int i = 0; i < totalPlayers; i++) {
+            long total = bestWithoutFinal[i] + prize[i][n];
+            if (total > answer) answer = total;
+        }
+        System.out.println(answer);
+        sc.close();
     }
 }
